9b437bf64c201e83aa9c1afb29f23f0801c7371f
takuseno <takuma.seno@gmail.com>
2018-09-15 09:47:24 +0900

    Add comment

--- test_latent.py
+++ test_latent.py
@@ -40,16 +40,19 @@ def main():
 
     image_rows = []
     for i in range(constants.LATENT_SIZE):
+        # change specific element of latent variable
         tiled_latent = np.tile(latent[0].copy(), (20, 1))
         tiled_latent[:,i] = latent_range
 
+        # reconstruct from latent variable
         reconst = generate_from_latent(tiled_latent)
 
-        # show reconstructed images
+        # tiling reconstructed images
         reconst_images = np.array(reconst * 255, dtype=np.uint8)
         reconst_tiled_images = tile_images(reconst_images, row=1)
         image_rows.append(reconst_tiled_images)
 
+    # show reconstructed images
     image_rows = tile_images(np.array(image_rows), row=constants.LATENT_SIZE)
     cv2.imshow('test'.format(i), image_rows)
 
