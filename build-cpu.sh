55891850200a104813eac5c45df008f36ce12835
takuseno <takuma.seno@gmail.com>
2018-10-10 22:57:56 +0900

    Add os conditioned scripts

--- build-cpu.sh
+++ build-cpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -e 's/{image}/tensorflow\/tensorflow:latest-gpu-py3/g' Dockerfile_template > Dockerfile
+sed -e 's/{image}/tensorflow\/tensorflow:latest-py3/g' Dockerfile_template > Dockerfile
 uid=$(id $whoamai | awk '{print $1}' | sed -e 's/uid=//g' | sed -e 's/(.*$//g')
 gid=$(id $whoamai | awk '{print $2}' | sed -e 's/gid=//g' | sed -e 's/(.*$//g')
 sudo docker build --build-arg uid=$uid --build-arg gid=$gid -t takuseno/beta-vae .
--- train.sh
+++ train.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-docker run --runtime nvidia -it --rm \
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  RUNTIME=""
+  _DISPLAY="$(ifconfig en0 | grep "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | sed -e "s/ netmask .*$//g" | sed -e "s/.*inet //g"):0"
+else
+  RUNTIME="--runtime nvidia"
+  _DISPLAY=$DISPLAY
+fi
+
+docker run -it --rm \
+    $RUNTIME \
     --shm-size=256m \
     -u app \
     -e QT_X11_NO_MITSHM=1 \
-    -e DISPLAY=$DISPLAY \
+    -e DISPLAY=$_DISPLAY \
     -v ${PWD}:/home/app \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     takuseno/beta-vae \
